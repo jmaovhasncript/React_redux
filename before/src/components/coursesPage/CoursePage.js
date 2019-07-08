0c3d60453c34f1871445d1773b2398655f7cf562
mkumar3 <mkumar3@exponential.com>
2019-07-08 11:15:05 +0530

    lesson 3.1 adding one timer redux configuration

--- before/src/components/coursesPage/CoursePage.js
+++ before/src/components/coursesPage/CoursePage.js
@@ -3,9 +3,36 @@ import React from "react";
 class CoursesPage extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      course: {
+        title: ""
+      }
+    };
   }
+  handleChange = event => {
+    const copyCourse = { ...this.state.course, title: event.target.value };
+    this.setState({
+      course: copyCourse
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    alert(event.target.value);
+  };
   render() {
-    return <h1>courses</h1>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Courses</h2>
+        <h3>Add Course </h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="save" />
+      </form>
+    );
   }
 }
 
